e48302c54aa8c0b9a3acc4fd5863c021a5dbe42e
Marcelo <mrbalbuena@hotmail.com>
2021-02-25 09:08:35 +0000

    Added Embed and logic for videos

--- frontend/components/GalleryLayout.tsx
+++ frontend/components/GalleryLayout.tsx
@@ -1,61 +1,43 @@
-import { Button, Card, Divider, Image, Placeholder } from 'semantic-ui-react'
+import { Button, Card, Divider, Image, Placeholder, Embed } from 'semantic-ui-react'
 import React, { Component } from 'react'
 
 type cardModel = {
   avatar: string;
-  date: string;
-  header: string;
-  description: string;
+  video: boolean;
 }
 
 const cards: cardModel[] = [
   {
-    avatar: 'https://react.semantic-ui.com/images/avatar/large/helen.jpg',
-    date: 'Joined in 2013',
-    header: 'Helen',
-    description: 'Primary Contact',
+    avatar: 'https://random.dog/03024628-188b-408e-a853-d97c9f04f903.jpg',
+    video: false
   },
   {
-    avatar: 'https://react.semantic-ui.com/images/avatar/large/matthew.png',
-    date: 'Joined in 2013',
-    header: 'Matthew',
-    description: 'Primary Contact',
+    avatar: 'https://random.dog/e654dc69-29a4-4eb6-ad2d-2c18e2c67eee.jpg',
+    video: false
   },
   {
-    avatar: 'https://react.semantic-ui.com/images/avatar/large/molly.png',
-    date: 'Joined in 2013',
-    header: 'Molly',
-    description: 'Primary Contact',
+    avatar: 'https://random.dog/62d87d11-bcdb-410f-8aee-324fe07f0c70.mp4',
+    video: true
   },
   {
-    avatar: 'https://react.semantic-ui.com/images/avatar/large/molly.png',
-    date: 'Joined in 2013',
-    header: 'Molly',
-    description: 'Primary Contact',
+    avatar: 'https://random.dog/3b5eae93-b3bd-4012-b789-64eb6cdaac65.png',
+    video: false
   },  
   {
-    avatar: 'https://react.semantic-ui.com/images/avatar/large/molly.png',
-    date: 'Joined in 2013',
-    header: 'Molly',
-    description: 'Primary Contact',
+    avatar: 'https://random.dog/a922da9a-437c-4400-9d94-f36ec2e5452c.mp4',
+    video: true
   },    
   {
-    avatar: 'https://react.semantic-ui.com/images/avatar/large/helen.jpg',
-    date: 'Joined in 2013',
-    header: 'Helen',
-    description: 'Primary Contact',
+    avatar: 'https://random.dog/f3923c95-bad4-4dac-8de6-5adc92e5944e.webm',
+    video: true
   },
   {
-    avatar: 'https://react.semantic-ui.com/images/avatar/large/matthew.png',
-    date: 'Joined in 2013',
-    header: 'Matthew',
-    description: 'Primary Contact',
+    avatar: 'https://random.dog/48a94bcb-75d2-46de-9f81-e37cfb574674.jpg',
+    video: false
   },
   {
-    avatar: 'https://react.semantic-ui.com/images/avatar/large/helen.jpg',
-    date: 'Joined in 2013',
-    header: 'Helen',
-    description: 'Primary Contact',
+    avatar: 'https://random.dog/8590d98b-5839-4ec7-9065-e3aa0c2d0763.mp4',
+    video: true
   }
 ]
 
@@ -82,40 +64,31 @@ export default class PlaceholderExampleCard extends Component {
 
         <Card.Group doubling itemsPerRow={4} stackable>
           {cards.map((card: cardModel) => (
-            <Card key={card.header}>
+            <Card >
               {loading ? (
                 <Placeholder>
                   <Placeholder.Image square />
                 </Placeholder>
-              ) : (
-                <Image src={card.avatar} />
+              ) : (card.video? (
+                <Embed
+                icon='right circle arrow'                
+                url= {card.avatar}
+                active={true}
+                autoplay={true}
+              />
+              ): (
+                <Image 
+                size='medium'
+                src={card.avatar} />
+                )
               )}
-
-              <Card.Content>
-                {loading ? (
-                  <Placeholder>
-                    <Placeholder.Header>
-                      <Placeholder.Line length='very short' />
-                      <Placeholder.Line length='medium' />
-                    </Placeholder.Header>
-                    <Placeholder.Paragraph>
-                      <Placeholder.Line length='short' />
-                    </Placeholder.Paragraph>
-                  </Placeholder>
-                ) : (
-                  <>
-                    <Card.Header>{card.header}</Card.Header>
-                    <Card.Meta>{card.date}</Card.Meta>
-                    <Card.Description>{card.description}</Card.Description>
-                  </>
-                )}
-              </Card.Content>
 
               <Card.Content extra>
                 <Button disabled={loading} primary>
                   Add
                 </Button>
                 <Button disabled={loading}>Delete</Button>
+                
               </Card.Content>
             </Card>
           ))}
