dce6f871283989ebf4da538718549360e72b0a59
Marcelo <mrbalbuena@hotmail.com>
2021-02-26 08:53:41 +0000

    Added video and autorun. Tidy up

--- frontend/components/GalleryLayout.tsx
+++ frontend/components/GalleryLayout.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Divider, Image, Placeholder, Embed } from 'semantic-ui-react'
+import { Button, Card, Divider, Image, Placeholder, Embed, Container } from 'semantic-ui-react'
 import React, { Component } from 'react'
 
 type cardModel = {
@@ -22,11 +22,11 @@ const cards: cardModel[] = [
   {
     avatar: 'https://random.dog/3b5eae93-b3bd-4012-b789-64eb6cdaac65.png',
     video: false
-  },  
+  },
   {
     avatar: 'https://random.dog/a922da9a-437c-4400-9d94-f36ec2e5452c.mp4',
     video: true
-  },    
+  },
   {
     avatar: 'https://random.dog/f3923c95-bad4-4dac-8de6-5adc92e5944e.webm',
     video: true
@@ -62,58 +62,33 @@ export default class PlaceholderExampleCard extends Component {
         </Button>
         <Divider />
 
-        <Card.Group doubling itemsPerRow={4} stackable>
-          {cards.map((card: cardModel) => (
-            <Card >
-              {loading ? (
-                <Placeholder>
-                  <Placeholder.Image square />
-                </Placeholder>
-              ) : (card.video? (
-                <Embed
-                icon='right circle arrow'                
-                url= {card.avatar}
-                active={true}
-                autoplay={true}
-              />
-              ): (
-                <Image 
-                size='medium'
-                src={card.avatar} />
-                )
-              )}
+        <Container>
 
-              <Card.Content extra>
-                <Button disabled={loading} primary>
-                  Add
-                </Button>
-                <Button disabled={loading}>Delete</Button>
-                
-              </Card.Content>
-            </Card>
-          ))}
-        </Card.Group>
+          <Card.Group doubling itemsPerRow={4} stackable>
+            {cards.map((card: cardModel) => (
+              <Card>
+                {loading ? (
+                  <Placeholder>
+                    <Placeholder.Image square />
+                  </Placeholder>
+                ) : (card.video ? (
+                  <video
+                    src={card.avatar}
+                    autoPlay={true}
+                    loop={true}
+                  />
+                ) : (
+                    <Image
+                      size='large'
+                      src={card.avatar} />
+                  )
+                  )}
+
+              </Card>
+            ))}
+          </Card.Group>
+        </Container>
       </>
     )
   }
 }
-
-
-/*
-function GalleryLayout() {
-  return (
-    <div className="flex-container">
-
-      <li className="flex-item">1</li>
-      <li className="flex-item">2</li>
-      <li className="flex-item">3</li>
-      <li className="flex-item">4</li>
-      <li className="flex-item">5</li>
-      <li className="flex-item">6</li>
-
-    </div>
-  );
-}
-
-export default GalleryLayout;
-*/
