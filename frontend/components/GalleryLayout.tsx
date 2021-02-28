cef9c38c13ca4e1fec441a04b3e4a0cce6486e71
Marcelo <mrbalbuena@hotmail.com>
2021-02-28 10:24:13 +0000

    tidy up code

--- frontend/components/GalleryLayout.tsx
+++ frontend/components/GalleryLayout.tsx
@@ -1,42 +1,42 @@
-import { Button, Card, Divider, Image, Placeholder, Embed, Container } from 'semantic-ui-react'
+import { Button, Card, Divider, Image, Placeholder, Container } from 'semantic-ui-react'
 import React, { Component } from 'react'
 
 type cardModel = {
-  avatar: string;
+  url: string;
   video: boolean;
 }
 
 const cards: cardModel[] = [
   {
-    avatar: 'https://random.dog/03024628-188b-408e-a853-d97c9f04f903.jpg',
+    url: 'https://random.dog/03024628-188b-408e-a853-d97c9f04f903.jpg',
     video: false
   },
   {
-    avatar: 'https://random.dog/e654dc69-29a4-4eb6-ad2d-2c18e2c67eee.jpg',
+    url: 'https://random.dog/e654dc69-29a4-4eb6-ad2d-2c18e2c67eee.jpg',
     video: false
   },
   {
-    avatar: 'https://random.dog/62d87d11-bcdb-410f-8aee-324fe07f0c70.mp4',
+    url: 'https://random.dog/62d87d11-bcdb-410f-8aee-324fe07f0c70.mp4',
     video: true
   },
   {
-    avatar: 'https://random.dog/3b5eae93-b3bd-4012-b789-64eb6cdaac65.png',
+    url: 'https://random.dog/3b5eae93-b3bd-4012-b789-64eb6cdaac65.png',
     video: false
   },
   {
-    avatar: 'https://random.dog/a922da9a-437c-4400-9d94-f36ec2e5452c.mp4',
+    url: 'https://random.dog/a922da9a-437c-4400-9d94-f36ec2e5452c.mp4',
     video: true
   },
   {
-    avatar: 'https://random.dog/f3923c95-bad4-4dac-8de6-5adc92e5944e.webm',
+    url: 'https://random.dog/f3923c95-bad4-4dac-8de6-5adc92e5944e.webm',
     video: true
   },
   {
-    avatar: 'https://random.dog/48a94bcb-75d2-46de-9f81-e37cfb574674.jpg',
+    url: 'https://random.dog/48a94bcb-75d2-46de-9f81-e37cfb574674.jpg',
     video: false
   },
   {
-    avatar: 'https://random.dog/8590d98b-5839-4ec7-9065-e3aa0c2d0763.mp4',
+    url: 'https://random.dog/8590d98b-5839-4ec7-9065-e3aa0c2d0763.mp4',
     video: true
   }
 ]
@@ -73,14 +73,14 @@ export default class PlaceholderExampleCard extends Component {
                   </Placeholder>
                 ) : (card.video ? (
                   <video
-                    src={card.avatar}
+                    src={card.url}
                     autoPlay={true}
                     loop={true}
                   />
                 ) : (
                     <Image
                       size='large'
-                      src={card.avatar} />
+                      src={card.url} />
                   )
                   )}
 
